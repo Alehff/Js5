11bcba92a2215d689ba7dc5d3c465c758f6f2b84
Alehff <84363595+Alehff@users.noreply.github.com>
2021-11-25 09:49:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var numero1 = document.querySelector("#numero1")
+var numero2 = document.querySelector("#numero2")
+var resultado = document.querySelector("span")
+
+function soma() {
+    resultado.innerHTML = parseInt(numero1.value) + parseInt( numero2.value)
+}
+
+function subtração() {
+    resultado.innerHTML = numero1.value - numero2.value
+}
+
+function multiplicar() {
+    resultado.innerHTML = numero1.value * numero2.value
+}
+
+function dividir() {
+    resultado.innerHTML = numero1.value / numero2.value
+}
